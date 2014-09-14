94fd0ba0429a7a8f3b7d25b839b5a33aebdc3cbe
Sittikorn <sittikorn@sittikorns-MacBook-Air.local>
2014-09-14 19:20:39 +0700

    update store view

--- eBook/Controller/StoreViewController.m
+++ eBook/Controller/StoreViewController.m
@@ -12,22 +12,27 @@
 #import "Books.h"
 #import "SettingTableViewController.h"
 #import "FXImageView.h"
+#import "SwipeView.h"
 
-@interface StoreViewController () <iCarouselDataSource,iCarouselDelegate>
+@interface StoreViewController () <iCarouselDataSource,iCarouselDelegate,SwipeViewDataSource,SwipeViewDelegate>
 
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 
-@property (strong, nonatomic) UIImageView *titleView2;
+@property (strong, nonatomic) UIView *mainView;
+
+@property (strong, nonatomic) UIView *titleView;
+@property (strong, nonatomic) UIView *titleView2;
+@property (strong, nonatomic) UIView *titleView3;
 
 @property (nonatomic, strong) iCarousel *carousel1;
-@property (nonatomic, strong) iCarousel *carousel2;
+@property (nonatomic, strong) SwipeView *swipeView1;
+@property (nonatomic, strong) SwipeView *swipeView2;
 
 @end
 
 @implementation StoreViewController
 
-@synthesize carousel1;
-@synthesize carousel2;
+@synthesize swipeView1,swipeView2,carousel1;
 
 -(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -45,9 +50,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     [self addTitleView];
     [self addTop10Book];
-    
+    [self addCategoryView];
 }
 
 - (void)viewDidUnload
@@ -56,7 +62,8 @@
     
     //free up memory by releasing subviews
     self.carousel1 = nil;
-    self.carousel2 = nil;
+    self.swipeView1 = nil;
+    self.swipeView2 = nil;
 }
 
 - (void)dealloc
@@ -65,106 +72,15 @@
     //sending messages to a deallocated viewcontroller
     carousel1.delegate = nil;
     carousel1.dataSource = nil;
-    carousel2.delegate = nil;
-    carousel2.dataSource = nil;
+    swipeView1.delegate = nil;
+    swipeView1.dataSource = nil;
+    swipeView2.delegate = nil;
+    swipeView2.dataSource = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)addTitleView {
-    
-    //add background
-    UIImageView *titleView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, 300)];
-    titleView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    titleView.backgroundColor = [[UIColor yellowColor]colorWithAlphaComponent:0.7];
-    [self.scrollView addSubview:titleView];
-    
-    
-    //create carousel
-    carousel1 = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, 300)];
-    carousel1.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    carousel1.type = iCarouselTypeRotary;
-    carousel1.delegate = self;
-    carousel1.dataSource = self;
-    
-    //add carousel to view
-    [self.scrollView addSubview:carousel1];
-    
-}
-
-- (void)addTop10Book {
-    
-    //add label top10
-    UILabel *top10text = [[UILabel alloc]initWithFrame:CGRectMake(10, 165, self.scrollView.frame.size.width, 300)];
-    top10text.text = @"Top10";
-    top10text.font = [UIFont boldSystemFontOfSize:16];
-    [self.scrollView addSubview:top10text];
-    
-    //add button to change to see top 100
-    UIButton *seeAllButton = [[UIButton alloc]initWithFrame:CGRectMake(685, 306, 100, 18)];
-    [seeAllButton setTitle:@"See All >" forState:UIControlStateNormal];
-    seeAllButton.titleLabel.font = [UIFont boldSystemFontOfSize:14];
-    [seeAllButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [seeAllButton addTarget:self action:@selector(changeToSeeAll:) forControlEvents:UIControlEventTouchUpInside];
-    [self.scrollView addSubview:seeAllButton];
-    
-    //add background
-    self.titleView2 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 330, self.scrollView.frame.size.width, 200)];
-    self.titleView2.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    self.titleView2.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.7];
-    [self.scrollView addSubview:self.titleView2];
-    
-    //create carousel2
-    carousel2 = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, 300)];
-    carousel2.autoresizingMask = UIViewAutoresizingFlexibleWidth ;
-    carousel2.type = iCarouselTypeLinear;
-    carousel2.delegate = self;
-    carousel2.dataSource = self;
-    
-    //[self.scrollView addSubview:carousel2];
-}
-
--(IBAction)changeToSeeAll:(id)sender {
-    NSLog(@"come");
-}
-
-#pragma mark -
-#pragma mark iCarousel methods
-
-- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
-{
-    //return the total number of items in the carousel
-    if (carousel == carousel1) {
-        return 8;
-    } else {
-        return 5;
-    }
-}
-
-- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
-{
-    
-    if (view == nil)
-    {
-        FXImageView *imageView = [[FXImageView alloc] initWithFrame:CGRectMake(0, 0, 400.0f, 400.0f)];
-        imageView.contentMode = UIViewContentModeScaleAspectFit;
-        imageView.asynchronous = YES;
-        imageView.reflectionScale = 0.5f;
-        imageView.reflectionAlpha = 0.25f;
-        imageView.reflectionGap = 10.0f;
-        imageView.shadowOffset = CGSizeMake(0.0f, 0.9f);
-        imageView.shadowBlur = 5.0f;
-        imageView.cornerRadius = 10.0f;
-        view = imageView;
-    }
-    
-    //set image
-    ((FXImageView *)view).image = [UIImage imageNamed:@"book1.jpg"];
-    
-    return view;
 }
 
 - (void) curentUserSystem {
@@ -179,28 +95,197 @@
     }
 }
 
+- (void)addTitleView {
+    
+    self.mainView = [[UIView alloc]initWithFrame:self.scrollView.bounds];
+    [self.scrollView addSubview:self.mainView];
+    
+    //add background
+    self.titleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, 300)];
+    self.titleView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.titleView.backgroundColor = [[UIColor yellowColor]colorWithAlphaComponent:0.7];
+    
+    //create carousel
+    carousel1 = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, 300)];
+    carousel1.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    carousel1.type = iCarouselTypeRotary;
+    carousel1.clipsToBounds = YES;
+    carousel1.delegate = self;
+    carousel1.dataSource = self;
+    
+    //add carousel to view
+    [self.titleView addSubview:carousel1];
+    [self.mainView addSubview:self.titleView];
+    
+}
+
+- (void)addTop10Book {
+    
+    //add label top10
+    UILabel *top10text = [[UILabel alloc]initWithFrame:CGRectMake(10, 165, self.scrollView.frame.size.width, 300)];
+    top10text.text = @"Top10";
+    top10text.font = [UIFont boldSystemFontOfSize:16];
+    [self.mainView addSubview:top10text];
+    
+    //add button to change to see top 20
+    UIButton *seeAllButton = [[UIButton alloc]initWithFrame:CGRectMake(685, 306, 100, 18)];
+    [seeAllButton setTitle:@"See All >" forState:UIControlStateNormal];
+    seeAllButton.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+    [seeAllButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [seeAllButton addTarget:self action:@selector(changeToSeeAll:) forControlEvents:UIControlEventTouchUpInside];
+    [self.mainView addSubview:seeAllButton];
+    
+    //add background
+    self.titleView2= [[UIView alloc] initWithFrame:CGRectMake(0, 330, self.scrollView.frame.size.width, 200)];
+    self.titleView2.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.titleView2.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.1];
+    
+    
+    swipeView1 = [[SwipeView alloc] initWithFrame:CGRectMake(0,0, self.titleView2.frame.size.width, self.titleView2.frame.size.height)];
+    swipeView1.alignment = SwipeViewAlignmentEdge;
+    swipeView1.pagingEnabled = YES;
+    swipeView1.itemsPerPage = 10;
+    swipeView1.truncateFinalPage = YES;
+    swipeView1.delegate = self;
+    swipeView1.dataSource = self;
+    
+    [self.titleView2 addSubview:swipeView1];
+    [self.mainView addSubview:self.titleView2];
+}
+
+- (void)addCategoryView {
+    
+    //add Category Label
+    UILabel *top10text = [[UILabel alloc]initWithFrame:CGRectMake(10.0f, 400.0f, self.scrollView.frame.size.width, 300.0f)];
+    top10text.text = @"Browse Category";
+    top10text.font = [UIFont boldSystemFontOfSize:16];
+    [self.mainView addSubview:top10text];
+    
+    //add background
+    self.titleView3 = [[UIView alloc] initWithFrame:CGRectMake(0, 530.0f, self.scrollView.frame.size.width, 150)];
+    self.titleView3.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.titleView3.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.1];
+    
+    swipeView2 = [[SwipeView alloc] initWithFrame:CGRectMake(0,-16.0f, self.titleView2.frame.size.width, self.titleView2.frame.size.height)];
+    swipeView2.alignment = SwipeViewAlignmentEdge;
+    swipeView2.pagingEnabled = YES;
+    swipeView2.itemsPerPage = 5;
+    swipeView2.truncateFinalPage = YES;
+    swipeView2.delegate = self;
+    swipeView2.dataSource = self;
+    
+    [self.titleView3 addSubview:swipeView2];
+    [self.mainView addSubview:self.titleView3];
+    
+}
+
+#pragma mark -
+#pragma mark iCarousel methods
+
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+    return 8;
+}
+
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
+    
+    if (carousel == carousel1) {
+        
+        FXImageView *imageView = [[FXImageView alloc] initWithFrame:CGRectMake(0, 0, 420.0f, 280.0f)];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.asynchronous = YES;
+        imageView.reflectionScale = 0.5f;
+        imageView.reflectionAlpha = 0.25f;
+        imageView.reflectionGap = 10.0f;
+        imageView.shadowOffset = CGSizeMake(0.0f, 2.0f);
+        imageView.shadowBlur = 5.0f;
+        imageView.cornerRadius = 10.0f;
+        self.titleView = imageView;
+        
+        ((FXImageView *)self.titleView).image = [UIImage imageNamed:@"book1.jpg"];
+        
+        return self.titleView;
+        
+    }
+    return self.mainView;
+}
+
 - (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value
 {
-    //customize carousel display
-    switch (option)
+    if (option == iCarouselOptionSpacing)
     {
-        case iCarouselOptionSpacing:
-        {
-            if (carousel == carousel2)
-            {
-                //add a bit of spacing between the item views
-                return value * 1.05f;
-            }
-        }
-        default:
-        {
-            return value;
+        return value * 1.1f;
+    }
+    return value;
+}
+
+#pragma -
+#pragma swipeView
+
+- (NSInteger)numberOfItemsInSwipeView:(SwipeView *)swipeView
+{
+    if (swipeView == swipeView1) {
+        return 10;
+    } else {
+        return 5;
+    }
+}
+
+- (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    if (!view)
+    {
+        if (swipeView == swipeView1) {
+            
+            view = [[NSBundle mainBundle] loadNibNamed:@"StoreBookView" owner:self options:nil][0];
+            
+            
+            // set bookname
+            UILabel *bookName = (UILabel *)[view viewWithTag:101];
+            bookName.text = @"เมื่อวาน";
+            
+            // set author
+            UILabel *author = (UILabel *)[view viewWithTag:102];
+            author.text = @"ไปไหน";
+            
+            // set imagebook
+            __weak __unused UIImageView *bookImage = (UIImageView *) [view viewWithTag:201];
+            bookImage.image = [UIImage imageNamed:@"page.png"];
+            
+            
+            // set button to view detail
+            
+            UIButton *button = (UIButton *)[view viewWithTag:301];
+            button.frame = CGRectMake(0, 0, bookImage.frame.size.width, bookImage.frame.size.height);
+            [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+            [button addTarget:self action:@selector(swipeButton:) forControlEvents:UIControlEventTouchUpInside];
+            
+        } else if (swipeView == swipeView2) {
+            
+            view = [[NSBundle mainBundle] loadNibNamed:@"CategoryView" owner:self options:nil][0];
+            
+            // set imagebook
+            __weak __unused UIImageView *bookImage = (UIImageView *) [view viewWithTag:201];
+            bookImage.image = [UIImage imageNamed:@"book1.jpg"];
+
+            // set button to view detail
+            UIButton *button = (UIButton *)[view viewWithTag:301];
+            [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+            [button layer].cornerRadius = 10.0f;
+            [button addTarget:self action:@selector(swipeCategoryButton:) forControlEvents:UIControlEventTouchUpInside];
         }
     }
+    
+    return view;
 }
 
 #pragma mark -
 #pragma mark Button tap event
+
+-(IBAction)changeToSeeAll:(id)sender {
+    NSLog(@"come");
+}
 
 - (void)buttonTapped:(UIButton *)sender
 {
@@ -220,32 +305,16 @@
     }];
 }
 
+-(IBAction)swipeButton:(id)sender {
+    NSInteger index = [swipeView1 indexOfItemViewOrSubview:sender];
+    NSLog(@"%i",index);
+}
 
-/*
- - (void) didSelectItemFromCollectionView:(NSNotification *)notification
- {
- NSDictionary *cellData = [notification object];
- if (cellData)
- {
- //
- //[self.navigationController pushViewController:self.detailViewController animated:YES];
- 
- DetailBookViewController *controller = [[DetailBookViewController alloc] initWithNibName:@"DetailBookViewController" bundle:nil];
- controller.detailItem = cellData;
- 
- controller.view.frame = self.navigationController.view.bounds;
- [self.navigationController.view addSubview:controller.view];
- [self.navigationController addChildViewController:controller];
- 
- 
- 
- [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
- controller.view.alpha = 1.0;
- } completion:^(BOOL finished) {
- [controller didMoveToParentViewController:self];
- }];
- }
- }
- */
+-(IBAction)swipeCategoryButton:(id)sender {
+    NSInteger index = [swipeView2 indexOfItemViewOrSubview:sender];
+    NSLog(@"%i",index);
+}
+
+
 
 @end
--- eBook/Model/API/iCarousel/iCarousel.m
+++ eBook/Model/API/iCarousel/iCarousel.m
@@ -1,3 +1,4 @@
+
 //
 //  iCarousel.m
 //
